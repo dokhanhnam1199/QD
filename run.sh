9971f8d53fa786f53e16bd5c547059003c958f37
dokhanhnam1199 <dokhanhnam1199@gmail.com>
2025-07-01 17:02:29 +0000

    update

--- run.sh
+++ run.sh
@@ -5,13 +5,3 @@ export GEMINI_API_KEY="AIzaSyDWz_8AG8ir2JnMbK_fzFl4H66epIQkqFs"
 
 # Run Python scripts
 python3 main.py problem=bpp_online algorithm=hsevo-qd 
-python3 main.py problem=bpp_online algorithm=reevo-qd 
-python3 main.py problem=bpp_online algorithm=eoh-qd
-
-# Set Gemini API key
-export GEMINI_API_KEY="AIzaSyDfvAYFsYjunymV2TaCVzblh_BFz0B1G7M"
-
-# Run Python scripts
-python3 main.py problem=bpp_online algorithm=hsevo-qd 
-python3 main.py problem=bpp_online algorithm=reevo-qd 
-python3 main.py problem=bpp_online algorithm=eoh-qd  
