f75cd826a6272cc841cbbab85d14341e75f1ad41
dokhanhnam1199 <dokhanhnam1199@gmail.com>
2025-07-31 16:23:07 +0700

    update model

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export NVIDIA_NIM_API_KEY="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"
-# export GEMINI_API_KEY="AIzaSyAOsThfG73iYOo3u3cx1v42lCg1Uq9I4g4"
+# export NVIDIA_NIM_API_KEY="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"
+export GEMINI_API_KEY="AIzaSyAOsThfG73iYOo3u3cx1v42lCg1Uq9I4g4"
 python3 main.py problem=bpp_online algorithm=hsevo
 # python3 main.py problem=bpp_online algorithm=hsevo
 # python3 main.py problem=bpp_online algorithm=hsevo
