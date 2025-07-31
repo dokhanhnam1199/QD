2546d9ce8a93cecd4156b1a99cd97b9114e2235d
dokhanhnam1199 <dokhanhnam1199@gmail.com>
2025-07-31 16:31:20 +0700

    update config

--- run.sh
+++ run.sh
@@ -3,18 +3,18 @@
 # export NVIDIA_NIM_API_KEY="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"
 export GEMINI_API_KEY="AIzaSyAOsThfG73iYOo3u3cx1v42lCg1Uq9I4g4"
 python3 main.py problem=bpp_online algorithm=hsevo
-# python3 main.py problem=bpp_online algorithm=hsevo
-# python3 main.py problem=bpp_online algorithm=hsevo
+python3 main.py problem=bpp_online algorithm=hsevo
+python3 main.py problem=bpp_online algorithm=hsevo
 
 python3 main.py problem=bpp_online algorithm=hsevo-qd
-# python3 main.py problem=bpp_online algorithm=hsevo-qd
-# python3 main.py problem=bpp_online algorithm=hsevo-qd
+ython3 main.py problem=bpp_online algorithm=hsevo-qd
+python3 main.py problem=bpp_online algorithm=hsevo-qd
 
 python3 main.py problem=bpp_online algorithm=reevo
-# python3 main.py problem=bpp_online algorithm=reevo
-# python3 main.py problem=bpp_online algorithm=reevo
+python3 main.py problem=bpp_online algorithm=reevo
+python3 main.py problem=bpp_online algorithm=reevo
 
 python3 main.py problem=bpp_online algorithm=reevo-qd
-# python3 main.py problem=bpp_online algorithm=reevo-qd
-# python3 main.py problem=bpp_online algorithm=reevo-qd
+python3 main.py problem=bpp_online algorithm=reevo-qd
+python3 main.py problem=bpp_online algorithm=reevo-qd
 
